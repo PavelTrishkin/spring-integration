61ebaa66b7a348c3b7c8b51d0e641aeb05034b7a
Павел <p-trishkin1@mail.ru>
2020-11-08 05:38:07 +0300

    fix

--- src/main/java/ru/gb/trishkin/springintegration/domain/Sell.java
+++ src/main/java/ru/gb/trishkin/springintegration/domain/Sell.java
@@ -14,25 +14,30 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Builder
 @Entity
-@Table(name = "sells")
+@Table(name = "sells_tbl")
 public class Sell {
+    private static final String SEQUENCE_NAME = "sell_seq";
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = SEQUENCE_NAME)
+    @SequenceGenerator(name = SEQUENCE_NAME, sequenceName = SEQUENCE_NAME, allocationSize = 1)
     private Long id;
 
     private LocalDateTime created;
 
-    @Column(name = "order_id")
+    @Column(name = "order_id_fld")
     private Long orderId;
 
-    @Column(name = "user_name")
+    @Column(name = "user_name_fld")
     private String userName;
 
-    @Column(name = "product_title")
+    @Column(name = "product_title_fld")
     private String productTitle;
 
+    @Column(name = "amount_fld")
     private Long amount;
+
+    @Column(name = "sum_fld")
     private BigDecimal sum;
 
 }
